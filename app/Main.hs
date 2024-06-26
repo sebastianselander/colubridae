e57ca732b1fbedde50c5c5d0d0971ec150b38913
sebastianselander <sebastian.selander@gmail.com>
2024-06-26 12:21:59 +0200

    change how compiler works overarchingly???

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,19 @@
 module Main where
 
+import Compile (runCompile, showDebugs)
 import Data.Text.IO (hPutStrLn)
-import Frontend.Parser.Parse (parse)
-import Frontend.Renamer.Rn (rename)
-import Frontend.Typechecker.Tc (tc)
 import Relude
-import Text.Pretty.Simple
-import Types (pPretty)
-import Frontend.Error ( Report, report )
-import Frontend.StatementCheck (check)
-
-ok :: Report a => Either a b -> IO b
-ok (Left err) = hPutStrLn stderr (report err) *> exitFailure
-ok (Right a) = pure a
 
 main :: IO ()
 main = do
-    putStrLn "Compiling!"
-    [file] <- getArgs
-    input <- readFileBS file
+    [fileName] <- getArgs
+    contents <- decodeUtf8 <$> readFileBS fileName
 
-    putStrLn "\n=== Parse output ===\n"
-    res <- ok $ parse file (decodeUtf8 input)
-    putTextLn (pPretty res)
-    pPrint res
-
-    putStrLn "\n=== Rename output ===\n"
-    res <- ok $ rename res
-    putTextLn (pPretty res)
-    pPrint res
-
-    putStrLn "\n=== Return/break check output ===\n"
-    res <- ok $ check res
-    putTextLn (pPretty res)
-    pPrint res
-
-    putStrLn "\n=== Tc output ===\n"
-    res <- ok $ fst $ second report $ tc res
-    putTextLn (pPretty res)
-    pPrint res
+    case runCompile fileName contents of
+        (Left err, debugs) -> do
+            hPutStrLn stderr $ showDebugs debugs
+            hPutStrLn stderr err
+            exitFailure
+        (Right _, debugs) -> do
+            hPutStrLn stderr $ showDebugs debugs
+            exitSuccess
---
+++ src/Compile.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Compile where
+
+import Control.Arrow (left)
+import Control.Monad.Except (liftEither)
+import Control.Monad.Writer (tell, MonadWriter, Writer, runWriter)
+import Frontend.Error (Report (..))
+import Frontend.Parser.Parse (parse)
+import Frontend.Renamer.Rn (rename)
+import Frontend.StatementCheck (check)
+import Frontend.Typechecker.Tc (tc)
+import Relude hiding (concat, intercalate)
+import Data.Text (intercalate)
+import Text.Pretty.Simple (pShow)
+import Types (pPretty)
+
+data Phase = Parse | Rename | StCheck | TypeCheck
+    deriving Show
+
+data DebugOutput = Debug {phase :: Phase, pretty :: Text, normal :: Text}
+data DebugOutputs = Debugs {debugs :: [DebugOutput], warnings :: [Text]}
+
+instance Semigroup DebugOutputs where
+  (<>) (Debugs l1 r1) (Debugs l2 r2) = Debugs (l1 <> l2) (r1 <> r2)
+
+instance Monoid DebugOutputs where
+  mempty = Debugs [] []
+  mappend = (<>)
+
+log :: MonadWriter DebugOutputs m => DebugOutput -> [Text] -> m () 
+log debug warnings = do
+    tell (Debugs [debug] warnings)
+
+compile :: String -> Text -> ExceptT Text (Writer DebugOutputs) Text
+compile fileName fileContents = do
+    res <- liftEither $ left report $ parse fileName fileContents
+    log (Debug Parse (pPretty res) (toStrict $ pShow res)) []
+
+    res <- liftEither $ left report $ rename res
+    log (Debug Rename (pPretty res) (toStrict $ pShow res)) []
+
+    res <- liftEither $ left report $ check res
+    log (Debug StCheck (pPretty res) (toStrict $ pShow res)) []
+
+    _ <- case tc res of
+        (res, warnings) -> do
+            res <- liftEither $ left report res
+            log (Debug TypeCheck (pPretty res) (toStrict $ pShow res)) (fmap report warnings)
+            pure res
+
+    pure ""
+
+runCompile :: String -> Text -> (Either Text Text, DebugOutputs)
+runCompile fileName = runWriter . runExceptT . compile fileName 
+
+showDebug :: DebugOutput -> Text
+showDebug (Debug phase pretty normal) = 
+    unlines ["======== " <> show phase <> " output ========", "", pretty, "", normal]
+
+showDebugs :: DebugOutputs -> Text
+showDebugs (Debugs debugs warnings) = unlines [intercalate "\n" $ fmap showDebug debugs, intercalate "\n" warnings]
