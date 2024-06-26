8c3295a9d61d5b08ca3f381b62c46a0e4f71c427
sebastianselander <sebastian.selander@gmail.com>
2024-06-26 12:22:10 +0200

    make testsuite

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,34 @@
 module Main (main) where
 
+import Compile
+import Data.List (groupBy)
+import Relude
+import System.Directory (listDirectory)
+import System.FilePath (dropExtensions)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+    goods <-
+        ( fmap (\[a, b] -> (a, b))
+                . groupBy (\l r -> dropExtensions l == dropExtensions r)
+                . sort
+            )
+            . fmap ("test/good/" ++)
+            <$> listDirectory "test/good"
+
+    bads <-
+        ( fmap (\[a, b] -> (a, b))
+                . groupBy (\l r -> dropExtensions l == dropExtensions r)
+                . sort
+            )
+            . fmap ("test/bad/" ++)
+            <$> listDirectory "test/bad"
+    goods <- mapM (testFile isRight) goods
+    bads <- mapM (testFile isLeft) bads
+    unless (and goods && and bads) exitFailure
+    exitSuccess
+
+testFile :: (forall a b. Either a b -> Bool) -> (String, String) -> IO Bool
+testFile isEither (input, _) = do
+    content <- decodeUtf8 <$> readFileBS input
+    pure $ isEither $ fst $ runCompile input content
